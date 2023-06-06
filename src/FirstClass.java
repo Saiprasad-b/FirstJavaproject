989bc98f37800ec81563b8d18587a2269de3b6ed
Saiprasad.b <bowrojusaiprasad.com>
2023-06-06 11:44:24 +0530

    First Java Git

---
+++ src/FirstClass.java
@@ -0,0 +1,13 @@
+
+public class FirstClass {
+	 public void add() {
+	int a =14;
+	int b=13;
+	System.out.println(a+b);
+}
+	 public static void main(String[] args) {
+		 
+		 FirstClass s = new FirstClass();
+		 s.add();
+	 }
+}
